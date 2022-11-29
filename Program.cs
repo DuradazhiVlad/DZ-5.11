ea8167e0c39571ddccb19891725ea5b830276469
DuradazhiVlad <dvlad1995p@gmail.com>
2022-11-29 13:09:02 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Черга_до_лікаря;
+ object obd = new object();
+
+
+    for (int i = 0; i < 1; i++)
+    {
+        Thread thread = new Thread(_Human);
+        thread.Name = $"thread{i}";
+        thread.Start();
+Console.WriteLine();
+    }
+
+ static void _Human(object obd)
+{
+    
+        PriorityQueue<Human, int> priority = new PriorityQueue<Human, int>();
+        for (int i = 0; i < 10; i++)
+        {
+
+            priority.Enqueue(new Human(), i);
+
+        }
+
+   
+
+        for (int i = 0; i < 10; i++)
+        {
+
+
+            Thread.Sleep(1000);
+            Console.WriteLine($"delete{i}", priority.Dequeue());
+
+
+        }
+        for (int i = 3; i < 10; i++)
+        {
+            Console.WriteLine($"Peek{i}", priority.Peek());
+        }
+    
+    
+}
+
+
+
+
---
+++ men.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Черга_до_лікаря
+{
+    public class Human
+    {
+        public string name { get; set; }
+      
+       
+        
+        
+
+    }
+}
